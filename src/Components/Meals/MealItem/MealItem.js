87813de292fcffc1228c5f33157ee51798a11c22
runzboi <richesmetelewawon@gmail.com>
2022-11-08 15:40:11 +0100

    added the meals item components

---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./MealItem.module.css";
+const MealItem = () => {
+  return <div className={classes.meals}>MealList</div>;
+};
+
+export default MealItem;
